86557e1e647fa656b63616d7be757cc5c1b744ce
nahdakh <nahdakhoir20@gmail.com>
2024-04-24 13:49:14 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:VendorConnect/halaman/halaman_utama.dart';
+import 'package:VendorConnect/models/shop.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => JemsSupply(),
+        builder: (context, child) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(primarySwatch: Colors.amber),
+              home: const HomePage(),
+            ));
+  }
+}
